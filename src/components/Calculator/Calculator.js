d921a9bd479d05b56b9a454c5b414f646538c206
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-16 13:00:52 -0800

    [update]

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -3,30 +3,51 @@ import './Calculator.css';
 import { useState } from 'react';
 
 function Calculator() {
-    const [bill, setBill] = useState('')
-    const [tipPercent, setTipPercent] = useState('')
-    const [split, setSplit] = useState('')
+    const [bill, setBill] = useState(0)
+    const [tip, setTip] = useState(0)
+    const [split, setSplit] = useState(0)
+
+    const tip_to_pay = bill * (tip/100)
+    const bill_total = bill + tip_to_pay
+    const person_total = bill_total / split
+    const tip_per_person = (bill * tip_to_pay) / split
+
 
      return (
-       <form>
-         <input 
-           type="number"
-           value={bill}
-           onChange={(e) => setBill(e.target.value )}
-         />
-   
-         <input 
-           type="number"
-           value={tipPercent}
-           onChange={(e) => setTipPercent(e.target.value )}
-         />
+        <div className='container'>
+            <form>
+                <div className='billAmount'>Bill Amount:
+                <input 
+                type="number"
+                value={bill}
+                onChange={(e) => setBill(e.target.value )}
+                />
+                </div>
 
-         <input 
-           type="number"
-           value={split}
-           onChange={(e) => setSplit(e.target.value )}
-         />
-       </form>
+                <div className='tipPercent'>Tip Percent:
+                <input 
+                type="number"
+                value={tip}
+                onChange={(e) => setTip(e.target.value )}
+                />
+                </div>
+
+                <div className='numPeople'>Number of People:
+                <input 
+                type="number"
+                value={split}
+                onChange={(e) => setSplit(e.target.value )}
+                />
+                </div>
+            </form>
+            
+            <h2>Results:</h2>
+            <h3>Bill Total: ${bill_total.toFixed(2)}</h3>
+            <h3>Tip Amount: ${tip_to_pay.toFixed(2)}</h3>
+            <h3>Tip Per Person: ${tip_per_person.toFixed(2)}</h3>
+            <h3>Bill Total Per Person: ${person_total.toFixed(2)}</h3>
+       </div>
+       
      )
    }
 
