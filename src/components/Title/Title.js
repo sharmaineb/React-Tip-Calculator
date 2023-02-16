65a53ddf6cdcaf7bc426a2f6156258ef687c62e5
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-16 10:24:05 -0800

    [update] setup

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,6 +1,13 @@
 import './Title.css'
 
 function Title() {
+    return (
+        <div className='Title'>
+            <header>
+                <div className='Title-Subtitle'>Tip Calculator</div>
+            </header>
+        </div>
+    )
 
 }
 
